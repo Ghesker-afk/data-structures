bd3f3fd0f2f8d28c1a7acc806b9909c418d4a999
Ghesker-afk <spacexz12345@gmail.com>
2025-09-01 10:24:00 -0300

    fix(selection-sort.c): refactored the comments and added a briefly documentation about selectionSort function.

--- algorithms/selection-sort.c
+++ algorithms/selection-sort.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void SelectionSort(int A[], int n) {
-  for (int i = 0; i < n - 1; i++) { // we need to do n - 2 passes, because the last element (at index n - 1) is correctly placed when the execution ends.
+/** @brief function that implements the selection sort algorithm, which sorts an array of integers in increasing order.
+ * @param A array of integers
+ * @param n length of array A
+ */
+void selectionSort(int A[], int n) {
+  // we need to do n - 2 passes, because the last element 
+  // (at index n - 1) is correctly placed when iterations ends.
+  for (int i = 0; i <= n - 2; i++) { 
 
-    int indexMin = i; // elements from i till n - 1 are candidates
+    // elements from i till n - 1 are candidates to be the minimum element in the array
+    int indexOfMinimumElement = i; 
 
-    for (int j = i + 1; j < n; j++) {
-      if (A[j] < A[indexMin]) {
-        indexMin = j; // update the index of the minimum element
+    for (int j = i + 1; j <= n - 1; j++) {
+      if (A[j] < A[indexOfMinimumElement]) {
+        // update the index of the minimum element
+        indexOfMinimumElement = j; 
       } 
     }
 
     int temp = A[i];
-    A[i] = A[indexMin];
-    A[indexMin] = temp;
+    A[i] = A[indexOfMinimumElement];
+    A[indexOfMinimumElement] = temp;
   }
 }
 
 int main(void) {
   int A[] = {2, 50, 102, 40, 30};
-  SelectionSort(A, 5);
+
+  int sizeOfArray = sizeof(A) / sizeof(A[0]);
+  selectionSort(A, sizeOfArray);
   
   printf("Values:");
   for (int i = 0; i < 5; i++) {
