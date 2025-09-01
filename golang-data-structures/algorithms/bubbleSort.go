b671decf997c8bb537c5905ac69c6f285b11221d
Ghesker-afk <spacexz12345@gmail.com>
2025-09-01 11:17:08 -0300

    fix(bubbleSort.go): refactored some issues and added more comments explaining the logic behind the scenes.

--- golang-data-structures/algorithms/bubbleSort.go
+++ golang-data-structures/algorithms/bubbleSort.go
@@ -6,21 +6,30 @@ func swap(x, y *int) {
 	*y = temp
 }
 
-// Function that sorts an array in increasing order using bubble sort algorithm
-// The first (and only) parameter is the unsorted (or not) array of integers
-func BubbleSort(list []int) {
-	length := len(list)
+func BubbleSort(list []int, n int) {
+	// number of passes to be done in the array. After n - 1
+	// passes, we are guaraanted to be sorted.
+	for k := 1; k <= n-1; k++ {
+		flag := false
 
-	for k := 1; k <= length-1; k++ {
-		flag := 0
-		for i := 0; i <= length-k-1; i++ {
+		// code responsible for each pass in the array
+
+		// if i is equal to n - 1, which is the last element
+		// in the array, we will compare n - 1 with n - 2, but
+		// n - 2 is out of bound of our slice. To correct this,
+		// we will run this loop only til n - 2.
+		// for each pass, k increments, and then we will need to
+		// go only to n - k - 1 elements in the array (just non-
+		// ordered part).
+
+		for i := 0; i < n-k-1; i++ {
 			if list[i] > list[i+1] {
 				swap(&list[i], &list[i+1])
-				flag = 1
+				flag = true
 			}
 		}
 
-		if flag == 0 {
+		if !flag {
 			break
 		}
 	}
